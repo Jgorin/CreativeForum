d4f0426006712d5256fedcc2cc91043b83a0a5a6
Josh Gorin <Josh@Joshuas-MacBook-Pro-2.local>
2021-05-25 08:39:20 -0400

    Added backdrop to users, added edit backdrop

--- client/src/services/UserFetches.js
+++ client/src/services/UserFetches.js
@@ -1,5 +1,13 @@
-import { GET } from "./Fetch"
+import { GET, PATCH } from "./Fetch"
 
 export const fetchUser = (userId) => {
   return GET(`/users/${userId}`)
+}
+
+export const editUserBackdrop = (userId, payload) => {
+  let backdropInfo = new FormData()
+  for(const [key, value] of Object.entries(payload)){
+    backdropInfo.append(key, value)
+  }
+  return PATCH(`/users/${userId}/backdrop`, backdropInfo)
 }
--- server/src/routes/api/v1/usersRouter.js
+++ server/src/routes/api/v1/usersRouter.js
@@ -3,7 +3,6 @@ import passport from "passport";
 import { User } from "../../../models/index.js";
 import multer from "multer"
 import S3Wrapper from "../../services/S3Wrapper.js"
-//server/src/routes/services/S3Wrapper.js
 
 const usersRouter = new express.Router();
 let upload = multer();
@@ -12,8 +11,9 @@ usersRouter.post("/", upload.single('file'), async(req, res) => {
   const { email, password, passwordConfirmation, fileName, username } = req.body;
   try {
     const file = req.file
-    const response = await S3Wrapper.upload(file, fileName)
-    const avatar = response.Location
+    const s3FileName = `users/${username}/profile_pictures/${fileName}`
+    const response = await S3Wrapper.upload(file, s3FileName)
+    const avatar = JSON.stringify(response)
     const persistedUser = await User.query().insertAndFetch({ username, email, password, avatar });
     return req.login(persistedUser, () => {
       return res.status(201).json({ user: persistedUser });
@@ -33,6 +33,25 @@ usersRouter.get("/:id", async(req, res) => {
   }
   user.posts = await user.$relatedQuery("posts")
   return res.status(200).json({user: user})
+})
+
+usersRouter.patch("/:id/backdrop", upload.single('file'), async(req, res) => {
+  const { fileName } = req.body
+
+  try{
+    const file = req.file
+    const user = await User.query().findById(req.params.id)
+    if(user.backdrop != null){
+      S3Wrapper.delete(user.backdrop.Key)
+    }
+    const response = await S3Wrapper.upload(file, `users/${user.username}/backdrops/${fileName}`)
+    const backdrop = JSON.stringify(response)
+    const updatedUser = await user.$query().patchAndFetchById(user.id, { backdrop: backdrop })
+    return res.status(201).json({ backdrop: response })
+  }
+  catch(error){
+    return res.status(400).json({ errors: error })
+  }
 })
 
 export default usersRouter;
